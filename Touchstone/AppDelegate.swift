a5ab1954455f44abd9f6d60153988c24befa2b3e
Nicolas Camenisch <garnele007@gmail.com>
2023-02-08 19:43:09 -0500

    Initial Commit

---
+++ Touchstone/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Touchstone
+//
+//  Created by Nicolas Camenisch on 08.02.23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
